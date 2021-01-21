95acfaa7ffcfe9bee1e67325e84966586e9e86ad
Kelly Young <kkristineyoung@gmail.com>
2021-01-20 21:33:56 -0500

    Add form prompting user to enter 1, 2, or 3; photo is shown depending on answer

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
+  $("form#photoChoice").submit(function(event) {
+    event.preventDefault();
+    const num = parseInt($("#number").val());
+    if (num === 1) {
+      $(".picture1").show();
 
+    } else if (num === 2) {
+      $(".picture2").show();
+
+    } else if (num === 3) {
+      $(".picture3").show();
+    } else {
+      alert("Please enter 1, 2, or 3.");
+    }
+  });
 });
